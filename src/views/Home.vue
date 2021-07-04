54a00884a75805510fe26ec91a4d6d2bd72bf547
ebalo <e.balsamo000@gmail.com>
2021-07-04 06:48:18 +0200

    graphical updates, mailchimp integration

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="v3_46">
+		<div class="v21_5"></div>
+		<div class="v21_77">
+			<div class="v21_78">
+				<div class="v21_79"></div>
+				<div class="v21_80"></div>
+				<div class="v21_81"></div>
+				<div class="v21_82"></div>
+				<div class="v21_83"></div>
+				<div class="v21_84"></div>
+				<div class="v21_85"></div>
+				<div class="v21_86"></div>
+				<div class="v21_87"></div>
+			</div>
+		</div>
+		<span class="v21_24">Subscribe to the waiting list 🚀</span>
+		<div class="v21_25">
+			<div class="v21_26">
+				<div class="v21_27"></div>
+				<span class="v21_28">your@email.com</span>
+			</div>
+			<div class="v21_29"></div>
+			<div class="v21_30">
+				<div class="v21_31"></div>
+				<div class="v21_32"></div>
+			</div>
+		</div>
+		<span class="v21_33">Made with ♥ by Do Labs</span>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+	name: 'Home',
+	components: {
+		HelloWorld
+	}
 }
 </script>
